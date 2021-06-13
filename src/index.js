d59d329318e3841c42ad59d11461352525a996af
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-13 14:18:25 -0500

    tic tac toe works

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function Square(props) {
     <button className="square" onClick={props.onClick}> 
       {props.value}
     </button>
-  ); //no need to use this.props if props is just an argument param. 
+  ); 
+  //no need to use this.props if props is just an argument param. 
+  //also, since props.onClick is now a function variable (or whatever that's called), there's no need to use arrow function syntax
 }
   
   class Board extends React.Component {
@@ -15,13 +17,21 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
+      
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice(); //this creates a copy for immutability reasons
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O'; //this syntax seems common. I think it works like booleanValue ? ifBoolTrueDo : ifBoolFalseDo
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -34,7 +44,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next Player: ' + (this.state.xIsNext ? "X" : "O");
+      }
   
       return (
         <div>
@@ -75,6 +91,27 @@ function Square(props) {
     }
   }
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]; //these are all of the possible combinations of matching indexes that would indicate winning values of a 3x3 grid, 
+    //i.e. 0,1,2 represents the top row of the grid
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];//declares an array of [a = lines[i][0], b = lines[i][1] . . .)
+      //checks if squares[a] first to make sure that it's not 3 missing nulls. 
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   ReactDOM.render(
